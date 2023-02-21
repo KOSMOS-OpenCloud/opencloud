owncloud/ocis

sharpen eventhistory service
Signed-off-by: jkoberg <jkoberg@owncloud.com>

services/eventhistory/pkg/config/defaults/defaultconfig.go
@@ -19,6 +19,18 @@ func DefaultConfig() *config.Config {
 		Service: config.Service{
 			Name: "eventhistory",
 		},
+		Events: config.Events{
+			Endpoint:  "127.0.0.1:9233",
+			Cluster:   "ocis-cluster",
+			EnableTLS: false,
+		},
+		Store: config.Store{
+			Type: "inmemory",
+		},
+		GRPC: config.GRPCConfig{
+			Addr:      "127.0.0.1:0",
+			Namespace: "com.owncloud.api",
+		},
 	}
 }
 
@@ -40,6 +52,15 @@ func EnsureDefaults(cfg *config.Config) {
 		cfg.GRPCClientTLS = &shared.GRPCClientTLS{}
 		if cfg.Commons != nil && cfg.Commons.GRPCClientTLS != nil {
 			cfg.GRPCClientTLS = cfg.Commons.GRPCClientTLS
+		}
+	}
+
+	if cfg.GRPC.TLS == nil {
+		cfg.GRPC.TLS = &shared.GRPCServiceTLS{}
+		if cfg.Commons != nil && cfg.Commons.GRPCServiceTLS != nil {
+			cfg.GRPC.TLS.Enabled = cfg.Commons.GRPCServiceTLS.Enabled
+			cfg.GRPC.TLS.Cert = cfg.Commons.GRPCServiceTLS.Cert
+			cfg.GRPC.TLS.Key = cfg.Commons.GRPCServiceTLS.Key
 		}
 	}
 }

KOSMOS-OpenCloud/opencloud

services/eventhistory/pkg/config/defaults/defaultconfig.go
@@ -1,6 +1,8 @@
 package defaults
 
 import (
+	"time"
+
 	"github.com/owncloud/ocis/v2/ocis-pkg/shared"
 	"github.com/owncloud/ocis/v2/services/eventhistory/pkg/config"
 )
@@ -18,6 +20,19 @@ func DefaultConfig() *config.Config {
 	return &config.Config{
 		Service: config.Service{
 			Name: "eventhistory",
+		},
+		Events: config.Events{
+			Endpoint:  "127.0.0.1:9233",
+			Cluster:   "ocis-cluster",
+			EnableTLS: false,
+		},
+		Store: config.Store{
+			Type:         "mem",
+			RecordExpiry: 336 * time.Hour,
+		},
+		GRPC: config.GRPCConfig{
+			Addr:      "127.0.0.1:0",
+			Namespace: "com.owncloud.api",
 		},
 	}
 }
@@ -40,6 +55,15 @@ func EnsureDefaults(cfg *config.Config) {
 		cfg.GRPCClientTLS = &shared.GRPCClientTLS{}
 		if cfg.Commons != nil && cfg.Commons.GRPCClientTLS != nil {
 			cfg.GRPCClientTLS = cfg.Commons.GRPCClientTLS
+		}
+	}
+
+	if cfg.GRPC.TLS == nil {
+		cfg.GRPC.TLS = &shared.GRPCServiceTLS{}
+		if cfg.Commons != nil && cfg.Commons.GRPCServiceTLS != nil {
+			cfg.GRPC.TLS.Enabled = cfg.Commons.GRPCServiceTLS.Enabled
+			cfg.GRPC.TLS.Cert = cfg.Commons.GRPCServiceTLS.Cert
+			cfg.GRPC.TLS.Key = cfg.Commons.GRPCServiceTLS.Key
 		}
 	}
 }
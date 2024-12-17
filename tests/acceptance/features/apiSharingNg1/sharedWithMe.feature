charliec-rls/owncloud

test: share with Denied role in Personal and Project space

tests/acceptance/features/apiSharingNg1/sharedWithMe.feature
@@ -5232,3 +5232,67 @@ Feature: an user gets the resources shared to them
     And the json response should contain the following shares:
       | textfile.txt  |
       | FolderToShare |
+
+  @env-config
+  Scenario: check the response while sharing a folder with denied permission role (Personal Space)
+    Given using spaces DAV path
+    And the administrator has enabled the permissions role "Denied"
+    And user "Alice" has created folder "FolderToShare"
+    And user "Alice" has sent the following resource share invitation:
+      | resource        | FolderToShare |
+      | space           | Personal      |
+      | sharee          | Brian         |
+      | shareType       | user          |
+      | permissionsRole | Denied        |
+    When user "Brian" lists the shares shared with him using the Graph API
+    Then the HTTP status code should be "200"
+    And user "Brian" should have a share "FolderToShare" shared by user "Alice"
+
+  @env-config
+  Scenario: check the response while sharing a folder with denied permission role (Project Space)
+    Given using spaces DAV path
+    And the administrator has enabled the permissions role "Denied"
+    And the administrator has assigned the role "Space Admin" to user "Alice" using the Graph API
+    And user "Alice" has created a space "NewSpace" with the default quota using the Graph API
+    And user "Alice" has created a folder "FolderToShare" in space "NewSpace"
+    And user "Alice" has sent the following resource share invitation:
+      | resource        | FolderToShare      |
+      | space           | NewSpace           |
+      | sharee          | Brian              |
+      | shareType       | user               |
+      | permissionsRole | Denied             |
+    When user "Brian" lists the shares shared with him using the Graph API
+    Then the HTTP status code should be "200"
+    And user "Brian" should not have a share "FolderToShare" shared by user "Alice"
+
+  @env-config
+  Scenario: check the response while sharing a file with denied permission role (Personal Space)
+    Given using spaces DAV path
+    And the administrator has enabled the permissions role "Denied"
+    And user "Alice" has uploaded file with content "secure content" to "fileToShare.txt"
+    And user "Alice" has sent the following resource share invitation:
+      | resource        | fileToShare.txt |
+      | space           | Personal      |
+      | sharee          | Brian         |
+      | shareType       | user          |
+      | permissionsRole | Denied        |
+    When user "Brian" lists the shares shared with him using the Graph API
+    Then the HTTP status code should be "200"
+    And user "Brian" should not have a share "FolderToShare" shared by user "Alice"
+
+  @env-config
+  Scenario: check the response while sharing a file with denied permission role (Project Space)
+    Given using spaces DAV path
+    And the administrator has enabled the permissions role "Denied"
+    And the administrator has assigned the role "Space Admin" to user "Alice" using the Graph API
+    And user "Alice" has created a space "NewSpace" with the default quota using the Graph API
+    And user "Alice" has uploaded a file inside space "NewSpace" with content "some content" to "insideSpace.txt"
+    And user "Alice" has sent the following resource share invitation:
+      | resource        | insideSpace.txt      |
+      | space           | NewSpace           |
+      | sharee          | Brian              |
+      | shareType       | user               |
+      | permissionsRole | Denied             |
+    When user "Brian" lists the shares shared with him using the Graph API
+    Then the HTTP status code should be "200"
+    And user "Brian" should not have a share "FolderToShare" shared by user "Alice"

KOSMOS-OpenCloud/opencloud

tests/acceptance/features/apiSharingNg1/sharedWithMe.feature
@@ -5232,3 +5232,45 @@ Feature: an user gets the resources shared to them
     And the json response should contain the following shares:
       | textfile.txt  |
       | FolderToShare |
+
+  @env-config
+  Scenario Outline: check the permission access while sharing a resource with denied permission role (Personal Space)
+    Given using spaces DAV path
+    And the administrator has enabled the permissions role "Denied"
+    And user "Alice" has created folder "FolderToShare"
+    And user "Alice" has uploaded file with content "personal space" to "/textfile.txt"
+    And user "Alice" has sent the following resource share invitation:
+      | resource        | <resource> |
+      | space           | Personal   |
+      | sharee          | Brian      |
+      | shareType       | user       |
+      | permissionsRole | Denied     |
+    When user "Brian" lists the shares shared with him using the Graph API
+    Then the HTTP status code should be "200"
+    And user "Brian" should not have a share "<resource>" shared by user "Alice"
+    Examples:
+      | resource      |
+      | FolderToShare |
+      | textfile.txt  |
+
+  @env-config
+  Scenario Outline: check the response while sharing a resource with denied permission role (Project Space)
+    Given using spaces DAV path
+    And the administrator has enabled the permissions role "Denied"
+    And the administrator has assigned the role "Space Admin" to user "Alice" using the Graph API
+    And user "Alice" has created a space "NewSpace" with the default quota using the Graph API
+    And user "Alice" has created a folder "FolderToShare" in space "NewSpace"
+    And user "Alice" has uploaded a file inside space "NewSpace" with content "project space" to "textfile.txt"
+    And user "Alice" has sent the following resource share invitation:
+      | resource        | <resource> |
+      | space           | NewSpace   |
+      | sharee          | Brian      |
+      | shareType       | user       |
+      | permissionsRole | Denied     |
+    When user "Brian" lists the shares shared with him using the Graph API
+    Then the HTTP status code should be "200"
+    And user "Brian" should not have a share "<resource>" shared by user "Alice"
+    Examples:
+      | resource      |
+      | FolderToShare |
+      | textfile.txt  |